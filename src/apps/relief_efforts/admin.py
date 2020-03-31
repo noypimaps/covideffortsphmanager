bcb299077145de3404a12ddc9b75f7d0fab64e72
Ian Panganiban <lkp@irayanergies.com>
2020-03-31 20:52:06 +0800

    enabled list editing

--- src/apps/relief_efforts/admin.py
+++ src/apps/relief_efforts/admin.py
@@ -5,31 +5,37 @@ from .models import *
 class OrganizationsAdmin(admin.ModelAdmin):
     list_display = ('id','org_name', 'org_type', 'severity_urgency', 'status')
     search_fields = ('org_name', 'org_type', 'status', 'severity_urgency')
+    list_editable = ('org_name', 'org_type', 'severity_urgency', 'status')
 
 class ContactDetailsAdmin(admin.ModelAdmin):
     list_display = ('id','org', 'name', 'contact_number')
     search_fields = ('org', 'name', 'contact_number')
+    list_editable = ('org', 'name', 'contact_number')
 
 class BankDetailsAdmin(admin.ModelAdmin):
     list_display = ('id','contact_details', 'bank_name', 'account_number', 'account_number')
     search_fields = ('contact_details', 'bank_name', 'account_number', 'account_number')
+    list_editable = ('contact_details', 'bank_name', 'account_number', 'account_number')
 
 class PricingDetailsAdmin(admin.ModelAdmin):
     list_display = ('id','org', 'name_of_item', 'unit', 'unit_price')
     search_fields = ('org', 'name_of_item', 'unit', 'unit_price')
+    list_editable = ('org', 'name_of_item', 'unit', 'unit_price')
 
 class AddressDetailsAdmin(admin.ModelAdmin):
     list_display = ('id','org', 'city_municipality', 'province', 'region')
     search_fields = ('org', 'city_municipality', 'province', 'region')
+    list_editable = ('org', 'city_municipality', 'province', 'region')
 
 class OtherDetailsAdmin(admin.ModelAdmin):
     list_display = ('id','org', 'link_for_more_info', 'link_to_poster')
     search_fields = ('org', 'who_they_help', 'what_they_do')
+    list_editable = ('org', 'who_they_help', 'what_they_do')
 
 class ItemizedNeedsAdmin(admin.ModelAdmin):
     list_display = ('id','org', 'name_of_item', 'unit', 'received_inventories', 'total_need')
     search_fields = ('org', 'name_of_item', 'unit', 'received_inventories', 'total_need')
-
+    list_editable = ('org', 'name_of_item', 'unit', 'received_inventories', 'total_need')
 
 admin.site.register(ItemizedNeeds, ItemizedNeedsAdmin)
 admin.site.register(OtherDetails, OtherDetailsAdmin)
