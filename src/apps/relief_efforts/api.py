db97a530859a25697330d737e2a5149ff1f4ecc5
Ian Panganiban <lkp@irayanergies.com>
2020-04-01 08:21:28 +0800

    updated requirements.txt, added common commands to entrypoint.prod.sh, added pagination to api.py

--- src/apps/relief_efforts/api.py
+++ src/apps/relief_efforts/api.py
@@ -1,13 +1,20 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets, permissions, response
+from rest_framework import viewsets, permissions, response, pagination
 from .models import *
 from .serializers import *
+
+
+class StandardResultsSetPagination(pagination.PageNumberPagination):
+    page_size = 100
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
 
 
 class OrganizationsViewset(viewsets.ModelViewSet):
     queryset = Organizations.objects.all()
     serializer_class = OrganizationsSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    pagination_class = StandardResultsSetPagination
     filter_fields = '__all__'
 
 
@@ -15,6 +22,7 @@ class ContactDetailsViewset(viewsets.ModelViewSet):
     queryset = ContactDetails.objects.all()
     serializer_class = ContactDetailsSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    pagination_class = StandardResultsSetPagination
     filter_fields = '__all__'
 
 
@@ -22,6 +30,7 @@ class BankDetailsViewset(viewsets.ModelViewSet):
     queryset = BankDetails.objects.all()
     serializer_class = BankDetailsSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    pagination_class = StandardResultsSetPagination
     filter_fields = '__all__'
 
 
@@ -29,6 +38,7 @@ class PricingDetailsViewset(viewsets.ModelViewSet):
     queryset = PricingDetails.objects.all()
     serializer_class = PricingDetailsSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    pagination_class = StandardResultsSetPagination
     filter_fields = '__all__'
 
 
@@ -36,6 +46,7 @@ class AddressDetailsViewset(viewsets.ModelViewSet):
     queryset = AddressDetails.objects.all()
     serializer_class = AddressDetailsSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    pagination_class = StandardResultsSetPagination
     filter_fields = '__all__'
 
 
@@ -43,6 +54,7 @@ class ItemizedNeedsViewset(viewsets.ModelViewSet):
     queryset = ItemizedNeeds.objects.all()
     serializer_class = ItemizedNeedsSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    pagination_class = StandardResultsSetPagination
     filter_fields = '__all__'
 
 
@@ -50,4 +62,5 @@ class OtherDetailsViewset(viewsets.ModelViewSet):
     queryset = OtherDetails.objects.all()
     serializer_class = OtherDetailsSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    pagination_class = StandardResultsSetPagination
     filter_fields = '__all__'
